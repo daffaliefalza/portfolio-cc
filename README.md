733af8c508a8d54411872ae25e0e79f250cc140f
Daffa Liefalza <liefalzaa@gmail.com>
2023-07-29 06:36:01 +0700

    fixing stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio CC
+
+<a href="https://daffaliefalza.github.io/portfolio-cc/">Live Site</a>
--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const body = document.querySelector("body");
-const button = document.querySelector("#toggle");
+const btnToggle = document.querySelector(".toggle");
 
-function toggleDark() {
-  if (body.classList.contains("light-mode")) {
-    body.classList.remove("light-mode");
-    localStorage.setItem("theme", "dark-mode");
-  } else {
-    body.classList.add("light-mode");
-    localStorage.setItem("theme", "light-mode");
-  }
-}
+btnToggle.addEventListener("click", function () {
+  document.documentElement.classList.toggle("light-mode");
+});
 
-if (localStorage.getItem("theme") === "light-mode") {
-  body.classList.add("light-mode");
-}
+// gunakan syntax dibawah ini. Dibawah ini adalah Logic untuk menghilangkan bug, ketika di refresh akan tetap menyesuaikan warna theme nya.
+// const body = document.querySelector("body");
+// const button = document.querySelector("#toggle");
 
-document.querySelector("#toggle").addEventListener("click", toggleDark);
+// function toggleDark() {
+//   if (body.classList.contains("light-mode")) {
+//     body.classList.remove("light-mode");
+//     localStorage.setItem("theme", "dark-mode");
+//   } else {
+//     body.classList.add("light-mode");
+//     localStorage.setItem("theme", "light-mode");
+//   }
+// }
+
+// if (localStorage.getItem("theme") === "light-mode") {
+//   body.classList.add("light-mode");
+// }
+
+// document.querySelector("#toggle").addEventListener("click", toggleDark);
